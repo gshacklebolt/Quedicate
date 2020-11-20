7835fc730f849d9e417b1ab2821e38ed8cd76cb6
GShacklebolt <ghazali123456@gmail.com>
2020-11-21 01:23:41 +0530

    Minor Changes

--- common/footer.php
+++ common/footer.php
@@ -1,4 +1,7 @@
 <div class="footer" align="center">
-	Copyright Quedicate<br>
-	Designed and Developed by Ghazali & Md Kasif
+	<p>
+		Designed &amp; Developed by <a href="https://ghazali.ml/" style="color: aqua;" onMouseOver="this.style.color='#fff'" onMouseOut="this.style.color='aqua'">Ghazali</a> and <a href="http://mstar.000webhostapp.com/" style="color: aqua;" onMouseOver="this.style.color='#fff'" onMouseOut="this.style.color='aqua'">Md Kasif</a>
+	</p>
+	<p> Copyright &copy; <script>document.write(new Date().getFullYear());</script> | All Rights Reserved
+	</p>
 </div>
